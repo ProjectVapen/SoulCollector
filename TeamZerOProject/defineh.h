51a0127b831aaf8391beab24d317b6d65341a919
(kou) <nwuser@GM2140036.da.iwasaki.ac.jp>
2015-06-15 10:59:53 +0900

    コントローラーとタイトル描画したよん

--- TeamZerOProject/defineh.h
+++
@@ -1,2 +0,0 @@
-#define MFAIL(code,string) if(FAILED( code ) ) { MessageBox(0,string,"error",MB_OK); return E_FAIL; }
-#define MFALSE(code,string) if(!( code ) ) { MessageBox(0,string,"error",MB_OK); return E_FAIL; }
